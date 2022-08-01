a95a03252d66c0fa9cb04adade75c21c00dc6c31
Olena <olenazachkow@gmail.com>
2022-08-01 14:16:06 -0700

    test page design

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -4,14 +4,24 @@ import "./Test.css";
 const Test = () => {
   return (
     <div className="test-container">
-      <div className="test-question">
-        <h1>Test questions</h1>
+      <div className="answers-row">
+        <div className="test-question">
+          <h1>Test questions</h1>
+        </div>
+        <div className="test-answers-col">
+          <div className="test-answ">
+            answer 1 hfhfhhfhfhfhfhfhfhhfhfhhfhfhhhhhhhhhhhhhhhhhhhhhhhhhhh
+          </div>
+          <div className="test-answ">answer 2</div>
+        </div>
+        <div className="test-answers-col">
+          <div className="test-answ">answer 3</div>
+          <div className="test-answ">answer 4</div>
+        </div>
       </div>
-      <div className="test-answers">
-        <div className="test-answ">answer 1</div>
-        <div className="test-answ">answer 2</div>
-        <div className="test-answ">answer 3</div>
-        <div className="test-answ">answer 4</div>
+      <div className="buttons">
+        <button>previous</button>
+        <button>next</button>
       </div>
     </div>
   );
